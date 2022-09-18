6a69eb012901836f16b7101fcec957dba514baa9
manasVinod2703 <110780616+manasVinod2703@users.noreply.github.com>
2022-09-19 00:10:36 +0530

    again

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import Register from "./pages/Register";
 import Login from "./pages/Login";
 import Home from "./pages/Home";
-import {BrowserRouter,Routes,Route,Navigate} from 'react-router-dom'
+import {BrowserRouter,Routes,Route,Navigate,HashRouter} from 'react-router-dom'
 import { AuthContext } from "./context/AuthContext";
 import { useContext } from "react";
 
@@ -21,7 +21,7 @@ function App() {
    
 
   return (
-    <BrowserRouter>
+    <HashRouter>
          <Routes>
             <Route path="/">
                <Route index element = {<ProtectedRoute><Home/></ProtectedRoute>} />
@@ -29,7 +29,7 @@ function App() {
                <Route path="/register" element = {<Register/>} />
             </Route>
          </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
